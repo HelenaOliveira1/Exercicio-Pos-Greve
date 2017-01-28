9d7ac470e4b75c3878531e19169ac0401daa4b06
HelenaOliveira1 <helenna54319@gmail.com>
2017-01-28 19:40:32 -0200

    Update Questão 5.py

--- Questão 5.py
+++ Questão 5.py
@@ -6,5 +6,9 @@ for min in range(1, quantidade+1):
     minutos += 10
 
 vida = (anos*365)*minutos
+horas = vida/60
+dias = horas/24
+meses = dias/30
+anos = meses/12
 
-print("Fumando assim, você perdeu,", vida, "minutos da sua vida.")
+print("Fumando assim, você perdeu, %.2f anos da sua vida." %anos)
